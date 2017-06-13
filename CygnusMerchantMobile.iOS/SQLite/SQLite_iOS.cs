20e3b02ae34436e2209abfcb19cdd37965ad532c
IntelLigenEthics7AppsIonic <7appsionic@gmail.com>
2017-06-13 15:22:30 +0800

    June2017 Project
    Provided by Jacky

---
+++ CygnusMerchantMobile.iOS/SQLite/SQLite_iOS.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+using SQLite;
+using Xamarin.Forms;
+using XamarinForms.SQLite.iOS.SQLite;
+using XamarinForms.SQLite.SQLite;
+
+[assembly: Dependency(typeof(SQLite_iOS))]
+
+namespace XamarinForms.SQLite.iOS.SQLite
+{
+
+    public class SQLite_iOS : ISQLite
+    {
+
+        public SQLite_iOS()
+        {
+        }
+
+        public SQLiteConnection GetConnection()
+        {
+            var sqliteFilename = "MySQLiteDB.db3";
+            string documentsPath = Environment.GetFolderPath(Environment.SpecialFolder.Personal); // Documents folder
+            string libraryPath = Path.Combine(documentsPath, "..", "Library"); // Library folder
+            var path = Path.Combine(libraryPath, sqliteFilename);
+            // Create the connection
+            var conn = new SQLiteConnection(path);
+            // Return the database connection
+            return conn;
+        }
+
+        public SQLiteAsyncConnection GetAsyncConnection()
+        {
+            var sqliteFilename = "MySQLiteDB.db3";
+            string documentsPath = System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal); // Documents folder
+            var path = Path.Combine(documentsPath, sqliteFilename);
+            // Create the connection
+            var conn = new SQLiteAsyncConnection(path);
+            // Return the database connection
+            return conn;
+        }
+    }
+}
